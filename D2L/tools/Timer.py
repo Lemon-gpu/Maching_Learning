116f735afba8d71dbb44fd8509ac3f78b31b2749
Lemon <ss666666699@outlook.com>
2022-02-13 22:56:22 +0800

    第三章

---
+++ D2L/tools/Timer.py
@@ -0,0 +1,25 @@
+import time
+
+import numpy
+
+
+class Timer:
+    def __init__(self) -> None:
+        self.times = []
+        self.start()
+
+    def start(self) -> None:
+        self.tik = time.time()
+
+    def stop(self) -> float:
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self) -> float:
+        return numpy.average(self.times)
+
+    def sum(self) -> float:
+        return numpy.sum(self.times)
+    
+    def cum_sum(self) -> numpy.ndarray:
+        return numpy.cumsum(self.times)
